387af3372a5533b9fcc77deddfaf577579b60dc9
Grundiz <felix.felix2@hotmail.com>
2026-05-04 11:39:28 +0200

    När Main.java körs så visas ett bräde i terminalen och på ett nytt fönster

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import model.SameGameModel;
+import view.ConsoleView;
+import view.SwingView;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        SameGameModel model = new SameGameModel(10, 10);
+
+        ConsoleView console = new ConsoleView();
+        SwingView swing = new SwingView(model);
+
+        model.addObserver(console);
+        model.addObserver(swing);
+
+        model.newGame();
     }
 }
--- src/model/SameGameModel.java
+++ src/model/SameGameModel.java
@@ -1,1 +1,46 @@
 package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import view.GameObserver;
+
+public class SameGameModel {
+
+    private List<GameObserver> observers = new ArrayList<>();
+
+    private int[][] board;
+
+    public SameGameModel(int rows, int cols) {
+        board = new int[rows][cols];
+        generateBoard();
+    }
+
+    private void generateBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = (int)(Math.random() * 3); // 3 färger
+            }
+        }
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void addObserver(GameObserver o) {
+        observers.add(o);
+    }
+
+    public void notifyObservers() {
+        for (GameObserver o : observers) {
+            o.update(this);
+        }
+    }
+
+    // test
+    public void newGame() {
+        generateBoard();
+        notifyObservers();
+    }
+}
