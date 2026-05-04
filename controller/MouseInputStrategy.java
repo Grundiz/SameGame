8924c3d1eb12ea281c9295dfdccbe244726e4732
Auroran-Bilbly <maxen123@live.se>
2026-05-04 09:45:36 +0200

    Update MouseInputStrategy.java
    Mouse check event

---
+++ controller/MouseInputStrategy.java
@@ -0,0 +1,9 @@
+Public class GameController {
+        addMouseListener(new MouseAdapter() {   //Gör så att den kollar efter musen
+          public void mouseClicked(MouseEvent e) {
+    int r = CellPanel.this.row;
+    int c = CellPanel.this.col;
+
+         }
+       );
+    }
