f68993487f36be9e502aaa065e9a740688c75e73
Auroran-Bilbly <maxen123@live.se>
2026-05-04 17:58:08 +0200

    Update sounds.java
    fixar "classens namn", jag är fan dum i huvudet

--- src/Sounds/sounds.java
+++ src/Sounds/sounds.java
@@ -1,4 +1,9 @@
-Public class sounds {  // den accepterar wav-filer
+package Sounds;
+
+import java.io.File;
+import javax.sound.sampled.*;
+
+public class sounds {  // den accepterar wav-filer
 public static void playSound(String path) {   // detta är för enstaka ljud som tex move:
     try {
         AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File(path));
