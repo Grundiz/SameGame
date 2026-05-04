ded0e8aaaac461d58260d482caf7cbd5db472052
Auroran-Bilbly <maxen123@live.se>
2026-05-04 10:06:56 +0200

    Update MouseInputStrategy.java
    fixade koden och la till generisk update game

--- controller/MouseInputStrategy.java
+++ controller/MouseInputStrategy.java
@@ -1,9 +1,17 @@
-Public class GameController {
+public class MouseInputStrategy extends JPanel {
+    private int row, col;
+
+public mouseClicked(int row, int col) {
+        this.row = row;   //ska checka vilken box man klickar på
+        this.col = col;
+        
         addMouseListener(new MouseAdapter() {   //Gör så att den kollar efter musen
           public void mouseClicked(MouseEvent e) {
-    int r = CellPanel.this.row;
-    int c = CellPanel.this.col;
+          public void mouseClicked(MouseEvent e) {
+
+        GameController.removeBlocks(); // ska 
 
          }
        );
     }
+ }
