8f5e360a9e6254046116ddfc46ba4c4b95ca3a41
Grundiz <felix.felix2@hotmail.com>
2026-05-05 10:59:30 +0200

    Gjorde så att man inte kan ta bort 1 block

--- src/model/SameGameModel.java
+++ src/model/SameGameModel.java
@@ -60,10 +60,34 @@ public class SameGameModel {
         removeGroup(row, col - 1, target); // vänster
     }
 
+    private int countGroup(int row, int col, int target, boolean[][] visited){
+        // utanför
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
+            return 0;
+        }
+        //redan besökt
+        if (visited[row][col]) return 0;
+        //fel färg
+        if (board[row][col] != target) return 0;
+
+        visited[row][col] = true;
+
+        return 1
+        + countGroup(row + 1, col, target, visited)
+        + countGroup(row - 1, col, target, visited)
+        + countGroup(row, col + 1, target, visited)
+        + countGroup(row, col - 1, target, visited);
+    }
+
     public void removeBlock(int row, int col) { 
     int target = board[row][col];
 
     if (target == -1) return;
+
+    boolean[][] visited = new boolean[board.length][board[0].length];
+    int size = countGroup(row, col, target, visited);
+
+    if (size < 2) return;
 
     // ta bort hela gruppen
     removeGroup(row, col, target);
